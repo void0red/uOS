2e17d2b63b640cddbaeafbe4888c491726d2a7f2
void0red <void0red@gmail.com>
2020-01-15 15:32:55 +0800

    add map unmap get_map function

--- include/vmm.h
+++ include/vmm.h
@@ -3,26 +3,33 @@
 
 #include <types.h>
 #include <idt.h>
+#include <pmm.h>
 
-#define PG_OFFSET 0xc0000000
+#define PAGE_OFFSET 0xc0000000
 #define PG_P (1 << 0)
 #define PG_W (1 << 1)
 #define PG_U (1 << 2)
 
 #define PAGE_MASK 0xfffff000
 #define PGSIZE 0x1000
-#define PGD_SIZE (PGSIZE / 32)
-#define PTE_SIZE (PGSIZE / 32)
+#define PGD_COUNT (PGSIZE / sizeof(uint32_t))
+// #define PTE_SIZE (PGSIZE / 32)
 #define PGD_INDEX(x) (((x) >> 22) & 0x3ff)
 #define PTE_INDEX(x) (((x) >> 12) & 0x3ff)
 #define OFF_INDEX(x) ((x)&0xfff)
 
-#define MEM_SIZE (512 << 20)
+#define MEM_SIZE (100 << 20)
 #define PTE_COUNT (MEM_SIZE / (PGSIZE << 10))
 
-void init_vmm();
-static void page_fault(trapframe_t *frame);
-void switch_page(uint32_t page);
 
-extern uint32_t kernel_pgd[PGD_SIZE];
+static void page_fault(trapframe_t *frame);
+static void flush_pte(uint32_t va);
+
+void init_vmm();
+void switch_page(uint32_t page);
+void map(uint32_t *pgd, uint32_t va, uint32_t pa, uint32_t flags);
+void unmap(uint32_t *pgd, uint32_t va);
+uint32_t get_map(uint32_t *pgd, uint32_t va, uint32_t *pa);
+
+extern uint32_t kernel_pgd[PGD_COUNT];
 #endif
--- kernel/vmm.c
+++ kernel/vmm.c
@@ -1,20 +1,20 @@
 #include <vmm.h>
 
-uint32_t kernel_pgd[PGD_SIZE] __attribute__((aligned(PGSIZE)));
+uint32_t kernel_pgd[PGD_COUNT] __attribute__((aligned(PGSIZE)));
 static uint32_t kernel_pte[PTE_COUNT][PGSIZE] __attribute__((aligned(PGSIZE)));
 
 void init_vmm()
 {
-    for (uint32_t i = PGD_INDEX(PG_OFFSET), j = 0; i < PTE_COUNT + PGD_INDEX(PG_OFFSET); ++i, ++j)
+    for (uint32_t i = PGD_INDEX(PAGE_OFFSET), j = 0; i < PTE_COUNT + PGD_INDEX(PAGE_OFFSET); ++i, ++j)
     {
-        kernel_pgd[i] = ((uint32_t)kernel_pte[j] - PG_OFFSET) | PG_P | PG_W;
+        kernel_pgd[i] = ((uint32_t)kernel_pte[j] - PAGE_OFFSET) | PG_P | PG_W;
     }
     for (uint32_t i = 1; i < PTE_COUNT * PGSIZE; ++i)
     {
         ((uint32_t *)kernel_pte)[i] = (i << 12) | PG_P | PG_W;
     }
     register_interrupt_handler(14, page_fault);
-    switch_page((uint32_t)kernel_pgd - PG_OFFSET);
+    switch_page((uint32_t)kernel_pgd - PAGE_OFFSET);
 }
 
 static void page_fault(trapframe_t *frame)
@@ -61,4 +61,53 @@ static void page_fault(trapframe_t *frame)
 inline void switch_page(uint32_t page)
 {
     __asm__ volatile("mov %0, %%cr3" ::"r"(page));
+}
+
+static inline void flush_pte(uint32_t va)
+{
+    __asm__ volatile("invlpg (%0)" ::"a"(va));
+}
+
+void map(uint32_t *pgd, uint32_t va, uint32_t pa, uint32_t flags)
+{
+    uint32_t pgd_idx = PGD_INDEX(va);
+    uint32_t pte_idx = PTE_INDEX(va);
+    uint32_t *pte = (uint32_t *)(pgd[pgd_idx] & PAGE_MASK);
+    if (!pte)
+    {
+        pte = (uint32_t *)pmm_alloc_page();
+        pgd[pgd_idx] = (uint32_t)pte | PG_P | PG_W;
+        memset((uint8_t *)pte, 0, PGSIZE);
+    }
+    pte = (uint32_t *)((uint32_t)pte + PAGE_OFFSET);
+    pte[pte_idx] = (pa & PAGE_MASK) | flags;
+
+    flush_pte(va);
+}
+
+void unmap(uint32_t *pgd, uint32_t va)
+{
+    uint32_t pgd_idx = PGD_INDEX(va);
+    uint32_t pte_idx = PTE_INDEX(va);
+    uint32_t *pte = (uint32_t *)(pgd[pgd_idx] & PAGE_MASK);
+    pte = (uint32_t *)((uint32_t)pte + PAGE_OFFSET);
+    pte[pte_idx] = 0;
+
+    flush_pte(va);
+}
+
+uint32_t get_map(uint32_t *pgd, uint32_t va, uint32_t *pa)
+{
+    uint32_t pgd_idx = PGD_INDEX(va);
+    uint32_t pte_idx = PTE_INDEX(va);
+    uint32_t *pte = (uint32_t *)(pgd[pgd_idx] & PAGE_MASK);
+    if (!pte)
+        return 0;
+    pte = (uint32_t *)((uint32_t)pte + PAGE_OFFSET);
+    if (pte[pte_idx] != 0 && pa)
+    {
+        *pa = pte[pte_idx] & PAGE_MASK;
+        return 1;
+    }
+    return 0;
 }
